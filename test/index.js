6e1538304db4232106995135d494b1a8b3529fb0
Eugene Ware <eugene@noblesamurai.com>
2015-01-20 19:54:27 +1100

    binary test

--- test/index.js
+++ test/index.js
@@ -9,10 +9,10 @@ var expect = require('expect.js'),
 describe('buffer-file-stream', function() {
   var tempDir = path.join(__dirname, '..', 'temp');
   beforeEach(function (done) {
-    rimraf(path.join(tempDir, '*.txt'), done);
+    rimraf(path.join(tempDir, '*.*'), done);
   });
 
-  it('should be able to write a single buffer to a file', function(done) {
+  it('should be able to write text buffers to files', function(done) {
     this.timeout(0);
 
     var bfs = bufferFileStream(path.join(tempDir, 'out-%03d.txt'));
@@ -37,6 +37,34 @@ describe('buffer-file-stream', function() {
     rs.pipe(bfs);
     rs.push(new Buffer('Hello'));
     rs.push(new Buffer('World'));
+    rs.push(null);
+  });
+
+  it('should be able to write binary data to files', function(done) {
+    this.timeout(0);
+
+    var bfs = bufferFileStream(path.join(tempDir, 'out-%03d.dat'));
+    var rs = stream.Readable();
+    rs._read = function () {};
+
+    bfs.on('finish', function (data) {
+      var files = ['000', '001'].map(function (item) {
+        return path.join(tempDir, 'out-' + item + '.dat');
+      });
+
+      async.map(files, fs.readFile, function (err, results) {
+        var answer = [
+          new Buffer([0x12, 0x34, 0x56, 0x78]),
+          new Buffer([0x87, 0x65, 0x43, 0x21])
+        ];
+        expect(answer).to.eql(results);
+        done();
+      });
+    });
+
+    rs.pipe(bfs);
+    rs.push(new Buffer([0x12, 0x34, 0x56, 0x78]));
+    rs.push(new Buffer([0x87, 0x65, 0x43, 0x21]));
     rs.push(null);
   });
 });
